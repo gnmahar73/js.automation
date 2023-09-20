cfac2d5979e82d0662e625e5db8591740c376a5c
gnmahar73 <137486330+gnmahar73@users.noreply.github.com>
2023-09-20 12:28:28 +0000

    Few MoreChanges

---
+++ GN/mod.js
@@ -0,0 +1,8 @@
+let name="GHulam Nabi"
+age=29
+location="karachi"
+
+console.log(`hello my name is ${name} and i am ${age} year old living in ${location}`)
+
+const gameName=new String('Hitesh')
+const myName=new String('Ikram')
