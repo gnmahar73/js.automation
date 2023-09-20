b6bafe1a0cd5fd3ceddf31fe3f316b68e3eedb58
gnmahar73 <137486330+gnmahar73@users.noreply.github.com>
2023-09-20 12:29:55 +0000

    Fe

--- GN/test.js
+++ GN/test.js
@@ -1,1 +1,14 @@
-console.log('GN'+' '+ 'MAHAR')
+//console.log('GN'+' '+ 'MAHAR')
+/*
+let Name='GN'
+let Age='28'
+let email='test@.com'
+
+let Name='GNM'
+let Age='280'
+let email='test7777@.com'
+console.table([Name,Age,email])*/
+
+
+
+
