ac4c90501b583faac8d1b7cf5c4e5ed46a23869f
gnmahar73 <137486330+gnmahar73@users.noreply.github.com>
2023-10-03 09:14:29 +0000

    math

---
+++ GN/amount.js
@@ -0,0 +1,6 @@
+const amount=new Number(500)
+console.log(amount)
+
+//******** view hundreds in organized way */
+const hundreds=100000000
+/
--- GN/math.js
+++ GN/math.js
@@ -1,5 +1,18 @@
-const score=400
+/*const score=400
 console.log(score)
 const balance=new Number(400)
 console.log(balance)
 console.log(balance.toFixed(3))
+*/
+
+const amount=new Number(152.9195)
+
+//console.log(amount.toPrecision(3))
+
+//console.log(amount.toFixed(2))
+
+const salary=new Number(22/7)
+//console.log(salary.toPrecision(3))
+//console.log(salary.toExponential(3))
+
+//+++++++++++++++++++++++++  MATH  ***************************
