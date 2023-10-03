c1d8f2efc4041058e920b3de09440d52e381af99
gnmahar73 <137486330+gnmahar73@users.noreply.github.com>
2023-10-03 10:01:31 +0000

    all math related and random numbers tricks

--- GN/math.js
+++ GN/math.js
@@ -16,3 +16,18 @@ const salary=new Number(22/7)
 //console.log(salary.toExponential(3))
 
 //+++++++++++++++++++++++++  MATH  ***************************
+
+console.log(Math.PI)
+console.log(Math.round(2.3356))
+console.log(Math.floor(20.98))
+console.log(Math.ceil(20.98))
+console.log(Math.abs(-58))
+console.log(Math.pow(3,2))
+
+let a=8
+let b=2
+let c=15
+console.log(Math.pow(a,b))
+console.log(Math.cbrt(a))
+console.log(Math.min(a,b,c))
+console.log(Math.max(a,b,c))
