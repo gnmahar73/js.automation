a976764da86ae16dfd38be18caa97e5230821221
gnmahar73 <137486330+gnmahar73@users.noreply.github.com>
2023-10-05 11:44:37 +0000

    spread sytex

--- GN/object.js
+++ GN/object.js
@@ -77,17 +77,41 @@ hisName={}
 //--------------------------------------object concatenated by spread methods ------------------
 
 const obj1={
-    id:1,name:'a'
+    id1:1,name:'a'
 }
 const obj2={
-    id:11,name:'b'
+   id2:11,name:'b'
 }
 const obj3={
-    id:10,name:'c'
+    id3:10,name:'c'
 }
 
-const obj4={obj1,obj2,obj3}
-//console.log(obj4)
+// const obj4={obj1,obj2,obj3}
+// console.log(obj4)
 
 const obj5={...obj1,...obj2,...obj3}
-console.log(obj5)
+// console.log(obj5)
+
+// const myVehicle = {
+//     brand: 'Ford',
+//     model: 'Mustang',
+//     color: 'red'
+//   }
+  
+//   const updateMyVehicle = {
+//     type: 'car',
+//     year: 2021, 
+//     color1: 'yellow'
+//   }
+  
+//   const myUpdatedVehicle = {...myVehicle, ...updateMyVehicle}
+//  console.log(myUpdatedVehicle)
+
+P1={name:'b',age1:20}
+P2={name1:'babcabdhvb',age2:55}
+p3={name2:'dnfbad',age3:85}
+
+pro=Object.assign({},P1,P2,p3)
+// console.log(pro)
+pro1={...{},...P1,...P2,...p3}
+console.log(pro1)
